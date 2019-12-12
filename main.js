46cebf8e943523cb188b17744b59b14897bdbf1a
Kobba <abegakuto.13931@gmail.com>
2019-12-12 14:15:47 +0900

    s

--- main.js
+++ main.js
@@ -8,39 +8,8 @@ client.on('ready', () => {
     client.user.setActivity('!zh args');
 });
 
-const events = {
-    MESSAGE_REACTION_ADD: 'messageReactionAdd',
-    MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
-};
-
-client.on('raw', async event => {
-    if (!events.hasOwnProperty(event.t)) return;
-
-    const { d: data } = event;
-    const user = client.users.get(data.user_id);
-    const channel = client.channels.get(data.channel_id) || await user.createDM();
-
-    if (channel.messages.has(data.message_id)) return;
-
-    const message = await channel.fetchMessage(data.message_id);
-    const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
-    let reaction = message.reactions.get(emojiKey);
-
-    if (!reaction) {
-        const emoji = new Discord.Emoji(client.guilds.get(data.guild_id), data.emoji);
-        reaction = new Discord.MessageReaction(message, emoji, 1, data.user_id === client.user.id);
-    }
-
-    client.emit(events[event.t], reaction, user);
-});
-
 client.on('message', message => {
     let channel = message.channel;
-
-    if ((!message.content.startsWith(config.prefix) || message.author.bot) && channel.topic) {
-        channel.setName(`${channel.topic}-${multByteStringSlice(message.author.username, 6)}`);
-        return
-    }
 
     const args = message.content.slice(config.prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -52,17 +21,6 @@ client.on('message', message => {
             channel.send(pinyin(args.join()).join(' '));
         }
     }
-});
-
-client.on('messageReactionAdd', (reaction, user) => {
-    if (reaction.message.author.id !== user.id) {
-        let message = `${user.username} reacted with ${reaction.emoji.name} to\n${reaction.message.cleanContent}`;
-        reaction.message.author.send(message);
-    }
-});
-
-client.on('messageReactionRemove', (reaction, user) => {
-    // console.log(`${user.username} removed their "${reaction.emoji.name}" reaction.`);
 });
 
 client.login(config.token);
@@ -80,16 +38,4 @@ function strLength(strSrc) {
         }
     }
     return len;
-}
-
-function multByteStringSlice(str, strLimit) {
-    var isSlice = false;
-    while (strLength(str) > strLimit) {
-        str = str.slice(0, str.length - 1);
-        isSlice = true;
-    }
-    if (isSlice) {
-        str += '...';
-    }
-    return str;
 }
