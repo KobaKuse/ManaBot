dc0f5b89d22852d04c14ef038f1bf5ee2adf5ae2
Kobba <abegakuto.13931@gmail.com>
2019-12-23 19:55:19 +0900

    test param

--- main.js
+++ main.js
@@ -1,41 +1,64 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
-const pinyin = require('pinyin');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const config = require('./config.json')
+const pinyin = require('pinyin')
+const request = require('request')
+const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('!zh args');
-});
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setActivity('!zh args')
+})
 
 client.on('message', message => {
-    let channel = message.channel;
+    let channel = message.channel
 
-    const args = message.content.slice(config.prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+    const args = message.content.slice(config.prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
 
     if (command === 'pinyin' || command === 'zh') {
         if (!args.length) {
-            channel.send(`You didn't provide any arguments, ${message.author}!`);
+            channel.send(`You didn't provide any arguments, ${message.author}!`)
         } else {
-            channel.send(pinyin(args.join()).join(' '));
+            channel.send(pinyin(args.join()).join(' '))
+        }
+    } else if (command === "word" || command === "words") {
+        if (!args.length) {
+            channel.send(`You didn't provide any arguments, ${message.author}!`)
+        } else {
+            let options = {
+                uri:
+                    "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec"
+                ,
+                headers: {
+                    "Content-type": "application/json",
+                },
+                json: {
+                    "noun": "param1",
+                    "pronunciation": "param2",
+                    "meaning": "param3",
+                    "example": "param4",
+                    "time": "param5"
+                }
+            }
+            request.post(options, function (error, response, body) { })
+            channel.send(`args ${args[0]} ${args[1]} ${args[2]} ${args[3]} ${args[4]}`)
         }
     }
-});
+})
 
-client.login(config.token);
+client.login(config.token)
 
 function strLength(strSrc) {
-    len = 0;
-    strSrc = escape(strSrc);
-    for (i = 0; i < strSrc.length; i++ , len++) {
+    len = 0
+    strSrc = escape(strSrc)
+    for (i = 0 i < strSrc.length i++ , len++) {
         if (strSrc.charAt(i) == '%') {
             if (strSrc.charAt(++i) == 'u') {
-                i += 3;
-                len++;
+                i += 3
+                len++
             }
-            i++;
+            i++
         }
     }
-    return len;
+    return len
 }
