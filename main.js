1f3ccc42ef12f6b8efc6e0d474efced80f2806c6
Kobba <abegakuto.13931@gmail.com>
2019-12-23 21:03:22 +0900

    commit

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ const pinyin = require('pinyin')
 const request = require('request')
 const client = new Discord.Client()
 
+function getNowYMD() {
+    var dt = new Date();
+    var y = dt.getFullYear();
+    var m = ("00" + (dt.getMonth() + 1)).slice(-2);
+    var d = ("00" + dt.getDate()).slice(-2);
+    var result = y + "/" + m + "/" + d;
+    return result;
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
     client.user.setActivity('!zh args')
@@ -17,14 +26,14 @@ client.on('message', message => {
 
     if (command === 'pinyin' || command === 'zh') {
         if (!args.length) {
-            channel.send(`You didn't provide any arguments, ${message.author}!`)
+            channel.send(`You didn't provide any arguments.`)
         } else {
             channel.send(pinyin(args.join()).join(' '))
         }
     } else if (command === "word" || command === "words") {
         if (!args.length) {
-            channel.send(`You didn't provide any arguments, ${message.author}!`)
-        } else {
+            channel.send(`You didn't provide any arguments.`)
+        } else if (args[0]) {
             let options = {
                 uri:
                     "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec"
@@ -33,15 +42,15 @@ client.on('message', message => {
                     "Content-type": "application/json",
                 },
                 json: {
-                    "noun": "param1",
-                    "pronunciation": "param2",
-                    "meaning": "param3",
-                    "example": "param4",
-                    "time": "param5"
+                    "noun": args[0],
+                    "pronunciation": args[1],
+                    "meaning": args[2],
+                    "example": args[3],
+                    "time": getNowYMD()
                 }
             }
             request.post(options, function (error, response, body) { })
-            channel.send(`args ${args[0]} ${args[1]} ${args[2]} ${args[3]} ${args[4]}`)
+            channel.send(`args ${args[0]} ${args[1]} ${args[2]} ${args[3]} `, getNowYMD())
         }
     }
 })
