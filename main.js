5a716113880de52a1f59d9f520b868e1ed666db2
Kobba <abegakuto.13931@gmail.com>
2019-12-24 20:15:39 +0900

    wor

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ const pinyin = require('pinyin')
 const request = require('request')
 const client = new Discord.Client()
 
+const postWords = (args, message) => {
+    let options = {
+        uri:
+            "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec"
+        ,
+        headers: {
+            "Content-type": "application/json",
+        },
+        json: {
+            "noun": args[0],
+            "pron": args[1],
+            "meaning": args[2],
+            "example": args[3],
+            "time": getNowYMD()
+        }
+    }
+    request.post(options, function (error, response, body) { })
+    message.reply(`added **${args[0]}** to Dictionary`).then(r => r.delete(10000))
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
     client.user.setActivity('!zh args')
@@ -15,35 +35,51 @@ client.on('message', message => {
     const args = message.content.slice(config.prefix.length).split(/ +/)
     const command = args.shift().toLowerCase()
 
+    channel.startTyping()
+
     if (command === 'pinyin' || command === 'zh') {
         if (!args.length) {
             channel.send(`You didn't provide any arguments.`)
         } else {
             channel.send(pinyin(args.join()).join(' '))
         }
-    } else if (command === "word" || command === "words") {
+
+    } else if (command === 'word' || command === 'w') {
+        const filter = m => m.author.id === message.author.id
+
+        let postData = []
+
+        message.reply('Please type **Word or Sentence**... will expire in a minute').then(r => r.delete(10000))
+        channel.awaitMessages(filter, { max: 1, time: 600000 }).then(j => {
+            if (!j.first() || j.first().content === "-") {
+                return message.reply('**Word or Sentence** is blank').then(r => r.delete(10000))
+            }
+            message.reply('Please type **Pronunciation** of words... will expire in a minute').then(r => r.delete(10000))
+            channel.awaitMessages(filter, { max: 1, time: 600000 }).then(k => {
+                message.reply('Please type **Meaning** of words... will expire in a minute').then(r => r.delete(10000))
+                channel.awaitMessages(filter, { max: 1, time: 600000 }).then(l => {
+                    message.reply('Please type **Example** of words... will expire in a minute').then(r => r.delete(10000))
+                    channel.awaitMessages(filter, { max: 1, time: 600000 }).then(m => {
+                        postData[0] = j.first().content
+                        postData[1] = k.first() ? k.first().content : "-"
+                        postData[2] = l.first() ? l.first().content : "-"
+                        postData[3] = m.first() ? m.first().content : "-"
+                        postWords(postData, message)
+                    })
+                })
+            })
+        })
+    }
+
+    else if (command === "word" || command === "words") {
         if (!args.length || args[0].match('/')) {
             channel.send(`You use / if args is a blank.\nFormat: noun pron. meaning example.\nexample: /word 去 qù Go 我去日本`)
         } else {
-            let options = {
-                uri:
-                    "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec"
-                ,
-                headers: {
-                    "Content-type": "application/json",
-                },
-                json: {
-                    "noun": args[0],
-                    "pron": args[1] ? args[1] : "/",
-                    "meaning": args[2] ? args[2] : "/",
-                    "example": args[3] ? args[3] : "/",
-                    "time": getNowYMD()
-                }
-            }
-            request.post(options, function (error, response, body) { })
-            channel.send(`added **${args[0]}** to Dictionary.`)
+
         }
     }
+
+    channel.stopTyping()
 })
 
 client.login(config.token)
