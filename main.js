7ffe6287fe95d80d20ae6fb77d5a19bdf794dee0
Kobba <abegakuto.13931@gmail.com>
2019-12-25 19:53:20 +0900

    can post jp,zh,en

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const pinyin = require('pinyin')
 const request = require('request')
 const client = new Discord.Client()
 
-const postWords = (args, message) => {
+const postWords = (args, message, sheetUrl) => {
     let options = {
         uri:
-            "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec"
+            sheetUrl
         ,
         headers: {
             "Content-type": "application/json",
@@ -24,6 +24,33 @@ const postWords = (args, message) => {
     message.reply(`added **${args[0]}** to Dictionary`)
 }
 
+const reciveCommand = (message, channel, sheetUrl) => {
+    const filter = m => m.author.id === message.author.id
+    let postData = []
+    message.delete(65000)
+
+    channel.send('If it is blank please type **( - )** Please type **Word or Sentence**... will expire in a minute').then(r => r.delete(30000))
+    channel.awaitMessages(filter, { max: 1, time: 60000 }).then(j => {
+        if (!j.first() || j.first().content === "-") {
+            return channel.send('**Word or Sentence** was blank').then(r => r.delete(10000))
+        }
+        channel.send('Please type **Pronunciation** of words... will expire in a minute').then(r => r.delete(30000))
+        channel.awaitMessages(filter, { max: 1, time: 60000 }).then(k => {
+            channel.send('Please type **Meaning** of words... will expire in a minute').then(r => r.delete(30000))
+            channel.awaitMessages(filter, { max: 1, time: 60000 }).then(l => {
+                channel.send('Please type **Example** of words... will expire in a minute').then(r => r.delete(30000))
+                channel.awaitMessages(filter, { max: 1, time: 60000 }).then(m => {
+                    postData[0] = j.first().content
+                    postData[1] = k.first() ? k.first().content : "-"
+                    postData[2] = l.first() ? l.first().content : "-"
+                    postData[3] = m.first() ? m.first().content : "-"
+                    postWords(postData, message, sheetUrl)
+                })
+            })
+        })
+    })
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
     client.user.setActivity('!zh args')
@@ -31,7 +58,6 @@ client.on('ready', () => {
 
 client.on('message', message => {
     let channel = message.channel
-
     const args = message.content.slice(config.prefix.length).split(/ +/)
     const command = args.shift().toLowerCase()
 
@@ -39,39 +65,21 @@ client.on('message', message => {
 
     if (command === 'pinyin' || command === 'zh') {
         if (!args.length) {
-            channel.send(`You didn't provide any arguments.`)
+            return channel.send(`You didn't provide any arguments.`).then(r => r.delete(10000))
         } else {
             channel.send(pinyin(args.join()).join(' '))
         }
-
     } else if (command === 'word' || command === 'w') {
-        const filter = m => m.author.id === message.author.id
-
-        let postData = []
-
-        message.reply('If it is blank please type **(-)**').then(r => r.delete(60000))
-        message.reply('Please type **Word or Sentence**... will expire in a minute').then(r => r.delete(30000))
-        channel.awaitMessages(filter, { max: 1, time: 60000 }).then(j => {
-            if (!j.first() || j.first().content === "-") {
-                return message.reply('**Word or Sentence** is blank').then(r => r.delete(30000))
-            }
-            message.reply('Please type **Pronunciation** of words... will expire in a minute').then(r => r.delete(30000))
-            channel.awaitMessages(filter, { max: 1, time: 60000 }).then(k => {
-                message.reply('Please type **Meaning** of words... will expire in a minute').then(r => r.delete(30000))
-                channel.awaitMessages(filter, { max: 1, time: 60000 }).then(l => {
-                    message.reply('Please type **Example** of words... will expire in a minute').then(r => r.delete(30000))
-                    channel.awaitMessages(filter, { max: 1, time: 60000 }).then(m => {
-                        postData[0] = j.first().content
-                        postData[1] = k.first() ? k.first().content : "-"
-                        postData[2] = l.first() ? l.first().content : "-"
-                        postData[3] = m.first() ? m.first().content : "-"
-                        postWords(postData, message)
-                    })
-                })
-            })
-        })
+        if (args[0] === 'jp' || args[0] === 'j') {
+            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec")//JP
+        } else if (args[0] === 'zh' || args[0] === 'z') {
+            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbymvndbSXkDNPtwO6pn4FOW_zQ1eQy6hC2JLj7TaQ9zgMjFZSM/exec")//ZH
+        } else if (args[0] === 'en' || args[0] === 'e') {
+            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbzmPjhN7qss64ct5ppRY6FCr7HPr46N2BX4Pw5Y83TXNYPl4EW6/exec")//EN
+        } else {
+            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec")//JP
+        }
     }
-
     channel.stopTyping()
 })
 
