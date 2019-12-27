0eb336c76df58fc7b9bf2f5049298e788cd71e29
Kobba <abegakuto.13931@gmail.com>
2019-12-27 23:17:09 +0900

    -

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ const pinyin = require('pinyin')
 const request = require('request')
 const client = new Discord.Client()
 
-const postWords = (args, message, sheetUrl) => {
+const GAS_URL_JP = "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec",
+    GAS_URL_ZH = "https://script.google.com/macros/s/AKfycbymvndbSXkDNPtwO6pn4FOW_zQ1eQy6hC2JLj7TaQ9zgMjFZSM/exec",
+    GAS_URL_EN = "https://script.google.com/macros/s/AKfycbzmPjhN7qss64ct5ppRY6FCr7HPr46N2BX4Pw5Y83TXNYPl4EW6/exec"
+
+const SHEET_URL_JP = "https://docs.google.com/spreadsheets/d/1tbT1qslmvK1uqk9EAuGYyHVuRdibKrW7FCdSeAPupN4/edit?usp=sharing",
+    SHEET_URL_ZH = "https://docs.google.com/spreadsheets/d/1Re-W0qqaQPHzPUGgCHf0X51ER1aYBX8_z-rF5d3K67E/edit?usp=sharing",
+    SHEET_URL_EN = "https://docs.google.com/spreadsheets/d/1MmBdoduV2FbB2npoc0O13tAXhlkTeaQVQOEuE-o08jI/edit?usp=sharing"
+
+const postWords = (args, message, gasUrl) => {
     let options = {
         uri:
-            sheetUrl
+            gasUrl
         ,
         headers: {
             "Content-type": "application/json",
@@ -21,10 +29,18 @@ const postWords = (args, message, sheetUrl) => {
         }
     }
     request.post(options, function (error, response, body) { })
-    message.reply(`added **${args[0]}** to Dictionary`)
+    if (gasUrl === GAS_URL_JP) { //JP
+        message.reply(`added **${args[0]}** to JP.\nCheck: ${SHEET_URL_JP}`)
+    } else if (gasUrl === GAS_URL_ZH) { //ZH
+        message.reply(`added **${args[0]}** to ZH.\nCheck: ${SHEET_URL_ZH}`)
+    } else if (gasUrl === GAS_URL_EN) { //EN
+        message.reply(`added **${args[0]}** to EN.\nCheck: ${SHEET_URL_EN}`)
+    } else { //JP
+        message.reply(`added **${args[0]}** to JP.\nCheck: ${SHEET_URL_JP}`)
+    }
 }
 
-const reciveCommand = (message, channel, sheetUrl) => {
+const reciveCommand = (message, channel, gasUrl) => {
     const filter = m => m.author.id === message.author.id
     let postData = []
     message.delete(10000)
@@ -48,7 +64,7 @@ const reciveCommand = (message, channel, sheetUrl) => {
                     postData[1] = k.first() ? k.first().content : "-"
                     postData[2] = l.first() ? l.first().content : "-"
                     postData[3] = m.first() ? m.first().content : "-"
-                    postWords(postData, message, sheetUrl)
+                    postWords(postData, message, gasUrl)
                 })
             })
         })
@@ -75,13 +91,13 @@ client.on('message', message => {
         }
     } else if (command === 'word' || command === 'w') {
         if (args[0] === 'jp' || args[0] === 'j') {
-            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec")//JP
+            reciveCommand(message, channel, GAS_URL_JP)//JP
         } else if (args[0] === 'zh' || args[0] === 'z') {
-            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbymvndbSXkDNPtwO6pn4FOW_zQ1eQy6hC2JLj7TaQ9zgMjFZSM/exec")//ZH
+            reciveCommand(message, channel, GAS_URL_ZH)//ZH
         } else if (args[0] === 'en' || args[0] === 'e') {
-            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbzmPjhN7qss64ct5ppRY6FCr7HPr46N2BX4Pw5Y83TXNYPl4EW6/exec")//EN
+            reciveCommand(message, channel, GAS_URL_EN)//EN
         } else {
-            reciveCommand(message, channel, "https://script.google.com/macros/s/AKfycbz4FSe6iSlsQBfIBORc2DjkFQCy9_9vjbj4hgzc/exec")//JP
+            reciveCommand(message, channel, GAS_URL_JP)//JP
         }
     }
     channel.stopTyping()
