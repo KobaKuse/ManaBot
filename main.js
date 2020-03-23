d8cf14a5e1c76b299a9d24e4466cbb8c6f59367c
Kobba <abegakuto.13931@gmail.com>
2020-03-23 16:48:24 +0900

    /

--- main.js
+++ main.js
@@ -95,6 +95,10 @@ const reciveCommand = (message, channel, gasUrl) => {
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
     client.user.setActivity('arknights2.jp', { type: 'WATCHING' })
+
+    setInterval(() => {
+        client.channels.get(691221361116446752).send("!d bump")
+    }, 10 * 1000)
 })
 
 client.on('message', message => {
@@ -141,7 +145,3 @@ function getNowYMD() {
     return result;
 }
 
-setInterval(() => {
-    channel = client.channels.get(691221361116446752)
-    channel.send("!d bump")
-}, 10 * 1000)
